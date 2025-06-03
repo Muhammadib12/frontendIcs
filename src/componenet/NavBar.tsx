972e81da0540f37cb4f15346165f1952d8d93c7b
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-06-04 01:06:02 +0300

    Update NavBar.tsx

--- src/componenet/NavBar.tsx
+++ src/componenet/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import  { useEffect, useRef, useState } from "react";
 import styles from "./../styles/NavBar.module.css";
 import { Link } from "react-router-dom";
 import {

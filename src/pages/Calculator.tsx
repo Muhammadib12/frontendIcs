84958c64edd34a08d3c9e854fbed5a901b747015
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-06-04 01:29:33 +0300

    Update Calculator.tsx

--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -284,7 +284,7 @@ function Calculator() {
                   annualRate: valueRe,
                   prime,
                   fixed,
-                  variable,
+                
                 });
 
                 setResult(result1);

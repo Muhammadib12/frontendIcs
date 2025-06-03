979d921b527ae0560e7c6383aeb5fe26545c011e
Muhammadib12 <160234764+Muhammadib12@users.noreply.github.com>
2025-06-04 01:02:33 +0300

    Update Alerts.tsx

--- src/componenet/Alerts.tsx
+++ src/componenet/Alerts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import styles from "./../styles/Alerts.module.css";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
